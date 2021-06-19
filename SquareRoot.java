2781fddd65c2bc5d68e0b6cd1c56517c068266d2
Pratik Chaudhari <pratikc1020@gmail.com>
2021-06-19 19:42:52 +0530

    Added function to calculate square root

---
+++ SquareRoot.java
@@ -0,0 +1,39 @@
+/**
+ * This code calculates square root of non-negative numbers using newton's methods.
+ *
+ * @author Pratik Chaudhari
+ * @since 14/06/2021
+ */
+import java.util.Scanner;
+
+public class SquareRoot {
+
+    public static void main(String[] args) {
+
+        //integer N and a tolerance level L
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int number = sc.nextInt();
+        System.out.print("Enter the tolerance level: ");
+        double toleranceLevel = sc.nextDouble();
+
+        sqrt(number, toleranceLevel);
+    }
+    private static void sqrt(double num, double lev) {
+        double x = num;
+        double root;
+        int count = 0;
+        while (true)
+        {
+            count++;
+            // Calculate more closed x
+            root = 0.5 * (x + (num / x));
+            // Check for closeness
+            if (Math.abs(root - x) < lev)
+                break;
+            // Update root
+            x = root;
+        }
+        System.out.println("Output of the number using Newton's Method is: " +root);
+    }
+}
